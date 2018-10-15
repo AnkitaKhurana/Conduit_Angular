0b52d54aaed289812a4bab5ae5903c461e688400
Ankita Khurana <ankita0096@gmail.com>
2018-10-15 11:26:24 +0530

    Login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,10 @@ import { UserService } from '../shared/services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  get email(){ return this.form.get('email')}
+  get password(){ return this.form.get('password')}
+  
+  responseError: string;
   form: FormGroup;
   constructor(private userService : UserService,private formBuilder: FormBuilder) { }
   login(): void {
@@ -17,11 +20,12 @@ export class LoginComponent implements OnInit {
       {         
       },     
       error => {
-        console.log(error);
+        Object.keys(JSON.parse(error._body).errors).forEach((k) => this.responseError = k + ' ' + JSON.parse(error._body).errors[k])
       }
     )
   }
   ngOnInit() {
+    this.responseError = '';
     this.form = this.formBuilder.group({
       email: [null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(20)])],
       password: [null, Validators.required]
